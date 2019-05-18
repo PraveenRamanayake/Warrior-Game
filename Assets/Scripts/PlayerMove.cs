c084ec444ede3880a1cd1ad864c2cd271c88e919
unknown <rmnpbr@gmail.com>
2019-05-18 18:42:03 +0530

    commiting

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController charController;
+
+    public float movement_Speed = 3f;
+    public float gravity = 9.8f;
+    public float rotation_Speed = 0.15f;
+    public float rotateDegreesPerSecond = 180f;
+
+
+
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+   void Move()
+    {
+
+        if (Input.GetAxis(Axis.VERTICAL_AXIS) > 0)
+        {
+            Vector3 moveDirection = transform.forward;
+            moveDirection.y -= gravity * Time.deltaTime;
+            charController.Move(moveDirection * movement_Speed * Time.deltaTime);
+
+        }else if (Input.GetAxis(Axis.VERTICAL_AXIS) < 0)
+        {
+            Vector3 moveDirection = -transform.forward;
+            moveDirection.y -= gravity * Time.deltaTime;
+            charController.Move(moveDirection * movement_Speed * Time.deltaTime);
+
+        }
+    }
+
+    void Rotate()
+    {
+        Vector3 rotation_Direction = Vector3.zero;
+
+        if (Input.GetAxis(Axis.HORIZONTAL_AXIS) < 0)
+        {
+            rotation_Direction = transform.TransformDirection(Vector3.left);
+        }
+
+        if (Input.GetAxis(Axis.HORIZONTAL_AXIS) > 0)
+        {
+            rotation_Direction = transform.TransformDirection(Vector3.right);
+        }
+        
+        if (rotation_Direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(rotation_Direction), rotateDegreesPerSecond * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis {
+    public const string VERTICAL_AXIS = "Vertical";
+    public const string HORIZONTAL_AXIS = "Horizontal";
+
+
+}
+
